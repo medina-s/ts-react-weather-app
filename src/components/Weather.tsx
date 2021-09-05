c3ef7d56de7f6e737a061f80077a48d89f4538ec
Medina88 <Msydyka@iu.edu>
2021-09-05 11:29:48 -0400

    Completed challenge

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -3,58 +3,52 @@ import React from "react";
 type WeatherState = {
   
     coords: [number, number],
-    weather: object[],
+    weather: string,
     temp: number | null,
-    main: string
+    speed: string,
+    cityname: string
 }
 
 type WeatherDisplayProps={
-    weather: string
+    weather: string,
+    temp: number | null,
+    speed: string,
+    cityname: string
 }
-
-// type WeatherApi={
-//   weather: {main:string}[]
-//   main: {temp: number}
-// }
-
 
 class Weather extends React.Component<{}, WeatherState>{
     constructor(props: {}){
         super(props)
         this.state = {
           coords: [0, 0],
-          weather:[],
+          weather:"",
           temp: null,
-          main: ""
+          speed: "",
+          cityname: ""
         }
     }
-
       
       weatherfetch= () => {
           navigator.geolocation.getCurrentPosition((position) => {
             this.setState({
                 coords: [position.coords.latitude, position.coords.longitude],
               })
-              console.log(this.state.coords)
               fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.coords[0]}&lon=${this.state.coords[1]}&appid=c03b4026b8715cc7b0d9debdd70d763a`, {
                 method: 'GET'
               }).then(res => res.json())
               .then(res => {
-                console.log(res)
                 this.setState({
                   weather: res.weather[0].main,
-                  temp: res.main.temp
+                  temp: res.main.temp,
+                  speed: res.wind.speed,
+                  cityname: res.name
 
                 })
-                console.log(this.state.weather)
               }).catch((err) => console.log(err))
             });
             }
 
-    
-
     // componentWillMount(){ ------- deprecated - we put instead inside of Constructor!!!!!!
-    
 
     componentDidMount(){
       this.weatherfetch();
@@ -63,7 +57,7 @@ class Weather extends React.Component<{}, WeatherState>{
       render(){
         return(
           <div>
-            <WeatherDisplay weather={this.state.weather}  />
+            <WeatherDisplay weather={this.state.weather} speed={this.state.speed} temp={this.state.temp} cityname={this.state.cityname} />
           </div>
         )
       }
@@ -72,7 +66,10 @@ class Weather extends React.Component<{}, WeatherState>{
     const WeatherDisplay = (props:WeatherDisplayProps) => {
       return (
         <div>
-          <h1>{props.weather}</h1>
+          <h1>City Name: {props.cityname}</h1>
+          <h3>Weather: {props.weather}</h3>
+          <h3>Speed: {props.speed}</h3>
+          <h3>Temperature: {props.temp}</h3>
         </div>
       )
     }
